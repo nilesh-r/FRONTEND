a669ebc993ffbf65707b87e4cbd6143d1c9b6900
nilesh-r <nileshsahu8674@gmail.com>
2025-03-16 21:44:13 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: process.env.PORT || 4227,
+    port: process.env.PORT || 2000,
   },
   preview: {
     host: '0.0.0.0',
-    port: process.env.PORT || 4227,
+    port: process.env.PORT || 2000,
   }
 })
