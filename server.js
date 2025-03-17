b5299f65abac40997466dfa06f1743b7c5a577bf
nilesh-r <nileshsahu8674@gmail.com>
2025-03-17 12:48:05 +0530

    commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 2000;
+const PORT = process.env.PORT || 4000;
 
 // Serve static files from the dist directory
 app.use(express.static(path.join(__dirname, 'dist')));
